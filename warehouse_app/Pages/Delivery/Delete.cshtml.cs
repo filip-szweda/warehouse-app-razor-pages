b2dd0736dc12ebfa742947d0a38dad8cfdebd64b
FilipSzweda <filip.szweda1@gmail.com>
2023-11-27 21:34:24 +0100

    Implement user authorization

--- warehouse_app/Pages/Delivery/Delete.cshtml.cs
+++ warehouse_app/Pages/Delivery/Delete.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,7 @@ using warehouse_app.Data;
 
 namespace warehouse_app.Pages.Delivery
 {
+    [Authorize]
     public class DeleteModel : PageModel
     {
         private readonly warehouse_app.Data.ApplicationDbContext _context;
--- warehouse_app/Pages/Water/Edit.cshtml.cs
+++ warehouse_app/Pages/Water/Edit.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -10,6 +11,7 @@ using warehouse_app.Data;
 
 namespace warehouse_app.Pages.Water
 {
+    [Authorize]
     public class EditModel : PageModel
     {
         private readonly warehouse_app.Data.ApplicationDbContext _context;
