7720bc42db29e66e1933b8dc6f4c028524ddabde
FilipSzweda <filip.szweda1@gmail.com>
2023-11-28 20:59:14 +0100

    Add sale processing endpoint

--- warehouse_app/Services/Api.cs
+++ warehouse_app/Services/Api.cs
@@ -1,6 +1,7 @@
 ﻿using warehouse_app.Data;
 using Microsoft.EntityFrameworkCore;
 using warehouse_lib.DTO;
+using warehouse_lib.Model;
 
 namespace warehouse_app.Services
 {
@@ -58,6 +59,81 @@ namespace warehouse_app.Services
                 DeliveryDetailsIds = w.DeliveryDetails.Select(d => d.Id).ToList(),
                 SaleDetailsIds = w.SaleDetails.Select(s => s.Id).ToList()
             }).ToList();
+        }
+
+        public async Task BuyWaterAsync(warehouse_lib.DTO.BuyWater buyWater)
+        {
+            await using var transaction = await _context.Database.BeginTransactionAsync();
+            try
+            {
+                if (buyWater == null)
+                {
+                    return;
+                }
+                List<SaleDetails> saleDetails = new List<SaleDetails>();
+
+                var Sale = new Sale()
+                {
+                    Customer = buyWater.Customer,
+                    SaleDate = DateTime.Now,
+                    SaleDetails = saleDetails
+                };
+
+                await _context.Sales.AddAsync(Sale);
+                await _context.SaveChangesAsync();
+
+                foreach (var item in buyWater.BuyWaterDetails)
+                {
+                    if (item == null)
+                    {
+                        continue;
+                    }
+
+                    if (item.NumberOfBottles <= 0)
+                    {
+                        continue;
+                    }
+
+                    var water = await _context.Waters
+                        .Include(w => w.Packaging)
+                        .Include(w => w.Producer)
+                        .Include(w => w.Type)
+                        .Include(w => w.Cations)
+                        .Include(w => w.Anions)
+                        .Include(water => water.DeliveryDetails)
+                        .Include(water => water.SaleDetails)
+                        .FirstOrDefaultAsync(w => w.Name == item.Name && w.Type.Type == item.Type);
+
+                    if (water == null)
+                    {
+                        continue;
+                    }
+
+                    var saleDetail = new SaleDetails()
+                    {
+                        NumberOfBottles = item.NumberOfBottles,
+                        WaterId = water.Id,
+                        Water = water,
+                        SaleId = Sale.Id,
+                        Sale = Sale,
+                    };
+
+                    saleDetails.Add(saleDetail);
+                    await _context.SaleDetails.AddAsync(saleDetail);
+                    await _context.SaveChangesAsync();
+                }
+
+                if (saleDetails.Count == 0)
+                {
+                    return;
+                }
+
+                await transaction.CommitAsync();
+            }
+            catch (Exception e)
+            {
+                await transaction.RollbackAsync();
+            }
         }
     }
 }
--- warehouse_lib/DTO/BuyWaterDetails.cs
+++ warehouse_lib/DTO/BuyWaterDetails.cs
@@ -7,7 +7,7 @@ namespace warehouse_lib.DTO
     public class BuyWaterDetails
     {
         public string Name { get; set; } = default!;
-        public int Quantity { get; set; }
+        public int NumberOfBottles { get; set; }
         public string Type { get; set; } = default!;
     }
 }
